1f58b1da56eea3be678b11c051d3b9b18800b0c8
Dmitrijs Vasilcenko <48312748+cropez@users.noreply.github.com>
2022-11-03 10:56:53 +0200

    1.0v

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Header from "./Header";
+import { Route, Switch } from "react-router-dom";
+
+
+export default function App() {
+    return(<>
+    <Header /><Switch>
+            <Route exact from="/Home" render={props => <Home {...props}/>} />
+            <Route exact path="/contact" render={props => <Contact {...props}/>} />
+            <Route exact path="/about" render={props => <About {...props} />} />
+            
+        </Switch>
+        </>
+  );
+}
